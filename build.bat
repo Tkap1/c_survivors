a5c2ba7e6ba82bb2a04c3118388c13c78969b447
Javier Palacios <javijpa@hotmail.com>
2024-09-25 21:56:14 +0200

    ...

--- build.bat
+++ build.bat
@@ -1,13 +1,25 @@
 @echo off
-
 cls
+setlocal
+
+for %%a in (%*) do set "%%a=1"
+
+if not "%release%"=="1" if not "%optimized_debug%"=="1" set debug=1
+
+set cl_common=cl -nologo ..\src\main.c -W4 -I..\src\include
+set link_common=-link ..\SDL3.lib -INCREMENTAL:NO
+set cl_debug=-Od -Zi -Dm_debug=1 -MTd
+set cl_optimized_debug=-O2 -Zi -Dm_debug=1 -MTd
+set cl_release=-O2 -MT
+
+if "%debug%"=="1" echo [Building debug]
+if "%optimized_debug%"=="1" echo [Building optimized debug]
+if "%release%"=="1" echo [Building release]
 
 pushd build
-	cl -nologo ..\src\main.c -W4 -Od -Zi -I..\src\include -link ..\SDL3.lib
+	if "%debug%"=="1" %cl_common% %cl_debug% %link_common% || exit /b 1
+	if "%optimized_debug%"=="1" %cl_common% %cl_optimized_debug% %link_common% || exit /b 1
+	if "%release%"=="1" %cl_common% %cl_release% %link_common% || exit /b 1
 popd
 
-set error_level=%ERRORLEVEL%
 copy build\main.exe main.exe > NUL
-@REM if %error_level%==0 (
-@REM 	call run.bat
-@REM )
